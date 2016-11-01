9b2975266013be6fbe9f786f290edf1c16f6bca8
Oleg Orlov <mail@orlovoleg.com>
2016-11-01 18:26:31 +0300

    test more node envs

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
 var test = require("tap").test;
 var testBundle = require("./sample/bundle");
+var a = require("./sample/dir/a.js");
+var b = require("./sample/dir/b.js");
 
 test("glob-loader", function (t) {
   t.similar(testBundle, {
-    "a": "A",
-    "b": "B"
+    "a": a,
+    "b": b,
   });
   t.end()
 });
